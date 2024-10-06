28dd7a6349ffb3fe968ab45a8b1cd20dd3f460be
alwin.anto <alwin.anto@valuementor.com>
2024-10-06 13:50:42 +0530

    password field added in add user field

--- src/pages/UserManagement/component/NewUserModal.js
+++ src/pages/UserManagement/component/NewUserModal.js
@@ -1,21 +1,27 @@
 // src/components/NewUserModal.js
-import React from 'react';
-import { 
-  Dialog, 
-  DialogActions, 
-  DialogContent, 
-  DialogTitle, 
-  TextField, 
-  Button, 
-  Box, 
-  IconButton 
-} from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
+import React from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  Button,
+  Box,
+  IconButton,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 const NewUserModal = ({ open, handleClose }) => {
   return (
     <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
-      <DialogTitle sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <DialogTitle
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         Add New User
         <IconButton
           edge="end"
@@ -27,57 +33,70 @@ const NewUserModal = ({ open, handleClose }) => {
         </IconButton>
       </DialogTitle>
       <DialogContent>
-        <Box 
-          component="form" 
-          sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }} 
-          noValidate 
+        <Box
+          component="form"
+          sx={{ display: "flex", flexDirection: "column", gap: "10px" }}
+          noValidate
           autoComplete="off"
         >
-          <TextField 
-            autoFocus 
-            margin="dense" 
-            id="userCode" 
-            label="User Code" 
-            type="text" 
-            fullWidth 
-            variant="outlined" 
+          <TextField
+            autoFocus
+            margin="dense"
+            id="userCode"
+            label="User Code"
+            type="text"
+            fullWidth
+            variant="outlined"
             defaultValue=""
             size="small"
           />
-          <TextField 
-            margin="dense" 
-            id="name" 
-            label="Name" 
-            type="text" 
-            fullWidth 
-            variant="outlined" 
+          <TextField
+            margin="dense"
+            id="name"
+            label="Name"
+            type="text"
+            fullWidth
+            variant="outlined"
             defaultValue=""
             size="small"
           />
-          <TextField 
-            margin="dense" 
-            id="email" 
-            label="Email" 
-            type="email" 
-            fullWidth 
-            variant="outlined" 
+          <TextField
+            margin="dense"
+            id="email"
+            label="Email"
+            type="email"
+            fullWidth
+            variant="outlined"
             defaultValue=""
             size="small"
           />
-          <TextField 
-            margin="dense" 
-            id="phone" 
-            label="Phone No." 
-            type="tel" 
-            fullWidth 
-            variant="outlined" 
+          <TextField
+            margin="dense"
+            label="Password"
+            type="password"
+            fullWidth
+            autoComplete="off"
+            size="small"
+          />
+          <TextField
+            margin="dense"
+            id="phone"
+            label="Phone No."
+            type="tel"
+            fullWidth
+            variant="outlined"
             defaultValue=""
             size="small"
           />
         </Box>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose} variant="contained" color="primary" fullWidth>
+        <Button
+          onClick={handleClose}
+          variant="contained"
+          color="primary"
+          fullWidth
+        >
           Save
         </Button>
       </DialogActions>
